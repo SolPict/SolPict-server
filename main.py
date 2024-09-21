a8b6beb8cd057d01fc27a3c90de4a6b6b1224df3
Yangsubo <sheeep555@gmail.com>
2024-09-21 13:21:01 +0900

    Feat: Add mathematics expression OCR API

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import base64
+import requests
+
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from pydantic import BaseModel
@@ -51,4 +54,18 @@ class Image(BaseModel):
 
 @app.post("/problem/analyze")
 async def analyzeProblem(image: Image):
-    return {"message": "Hello World"}
+    base64_data = image.imageURI.split(",")[1]
+    image_data = base64.b64decode(base64_data)
+    output_file_path = "temp_image.jpg"
+
+    with open(output_file_path, "wb") as file:
+        file.write(image_data)
+
+    requests.post(
+        os.getenv("MATH_OCR_URL"),
+        files=[("file", ("temp_image.jpg", open("temp_image.jpg", "rb")))],
+        data={"enable_img_rot": True},
+        headers={"token": os.getenv("MATH_OCR_TOKEN")},
+    )
+
+    return {"message": "성공적으로 요청을 받았습니다."}
