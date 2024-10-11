3b9307f0ab5ec0e6a2093318ce2e4bb03c0b34d2
Yangsubo <sheeep555@gmail.com>
2024-10-11 16:25:04 +0900

    Refactor: Modularize router, database

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
 import os
-import base64
-import json
-import requests
+
+from app.database import db
 
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from motor.motor_asyncio import AsyncIOMotorClient
 from contextlib import asynccontextmanager
+
+from app.routers import users
+from app.routers import problems
+from app.routers import problem
 
 load_dotenv()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await startup_db_client(app)
+    await db.connect_to_database(os.getenv("DATABASE_URL"))
     yield
-    await shutdown_db_client(app)
+    await db.close_database_connection()
 
 
-async def startup_db_client(app):
-    try:
-        app.mongodb_client = AsyncIOMotorClient(os.getenv("DATABASE_URL"))
-        app.mongodb = app.mongodb_client.get_database("SolPict")
-        print("데이터베이스 연결에 성공하였습니다.🙆‍♂️")
-    except Exception as error:
-        print("데이터베이스 연결에 실패하였습니다.🙅‍♂️", error)
-
-
-async def shutdown_db_client(app):
-    app.mongodb_client.close()
-    print("데이터베이스 연결을 성공적으로 종료하였습니다.🙇‍♂️")
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(title="FastAPI", lifespan=lifespan)
 
 origins = [
-    os.getenv("CLIENT_URL"),
+    os.getenv("WEB_CLIENT_URL"),
 ]
 
 app.add_middleware(
@@ -48,39 +35,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-class Image(BaseModel):
-    imageURI: str
-
-
-@app.post("/problem/analyze")
-async def analyzeProblem(image: Image):
-    base64_data = image.imageURI.split(",")[1]
-    image_data = base64.b64decode(base64_data)
-    output_file_path = "temp_image.jpg"
-
-    with open(output_file_path, "wb") as file:
-        file.write(image_data)
-
-    response = requests.post(
-        os.getenv("MATH_OCR_URL"),
-        files={"file": open("temp_image.jpg", "rb")},
-        data={
-            "options_json": json.dumps(
-                {"math_inline_delimiters": ["$", "$"], "rm_spaces": True}
-            ),
-            "formats": ["text"],
-            "format_options": {
-                "latex_styled": {"transforms": ["rm_newlines", "rm_fonts"]}
-            },
-            "idiomatic_braces": True,
-        },
-        headers={
-            "app_id": os.getenv("MATH_OCR_ID"),
-            "app_key": os.getenv("MATH_OCR_KEY"),
-        },
-    )
-
-    print(response)
-
-    return {"message": "성공적으로 요청을 받았습니다."}
+app.include_router(users.router)
+app.include_router(problem.router)
+app.include_router(problems.router)
