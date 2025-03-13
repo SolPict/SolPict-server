e8153eccd905834ce32c9c88c151d866e361e2eb
Yangsubo <sheeep555@gmail.com>
2025-03-13 16:06:45 +0900

    Refactor: Improve MongoDB connection and simplify session/transaction handling
    Refactor: Simplify MongoManager with get_session and  Remove transaction context

--- app/database/mongo_manager.py
+++ app/database/mongo_manager.py
@@ -1,44 +1,34 @@
 import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo import WriteConcern, ReadPreference
-from pymongo.read_concern import ReadConcern
 
 
 class MongoManager:
     _instance = None
     _lock = asyncio.Lock()
 
-    def __new__(cls, path: str):
+    def __new__(cls):
         if not cls._instance:
             cls._instance = super().__new__(cls)
-            cls._instance._init_connection(path)
         return cls._instance
 
-    async def _init_connection(self, path: str):
+    async def init_connection(self, path: str):
+        if hasattr(self, "client"):
+            return
+
         async with self._lock:
-            self.client = AsyncIOMotorClient(path)
-            self.mongodb = self.client["SolPict"]
-            print("데이터베이스 연결에 성공하였습니다.🙆‍♂️")
+            if not hasattr(self, "client"):
+                self.client = AsyncIOMotorClient(path)
+                self.mongodb = self.client["SolPict"]
+                print("데이터베이스 연결 성공! 🙆‍♂️")
 
     async def close_database_connection(self):
-        if self.client:
+        if hasattr(self, "client"):
             self.client.close()
-            print("데이터베이스 연결을 종료하였습니다.🙇‍♂️")
+            del self.client
+            print("데이터베이스 연결 종료 🙇‍♂️")
 
-    async def __aenter__(self):
-        self.session = await self.client.start_session()
-        self.session.start_transaction(
-            read_concern=ReadConcern("majority"),
-            write_concern=WriteConcern("majority"),
-            read_preference=ReadPreference.PRIMARY,
-        )
-        return self.session
+    async def get_session(self):
+        if not hasattr(self, "client"):
+            raise RuntimeError("MongoDB 연결이 초기화되지 않았습니다.")
 
-    async def __aexit__(self, exc_type, exc_value, traceback):
-        if exc_type is not None:
-            await self.session.abort_transaction()
-            print("트랜잭션 롤백됨.")
-        else:
-            await self.session.commit_transaction()
-            print("트랜잭션 커밋됨.")
-        await self.session.end_session()
+        return await self.client.start_session()
