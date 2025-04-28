1ee1992f4110afbc1e74d67af3763fe8e3df2ff0
Yangsubo <sheeep555@gmail.com>
2025-04-28 22:30:55 +0900

    Chore: Optimize background tasks with async ping loop for HuggingFace

--- app/background/ping_model.py
+++ app/background/ping_model.py
@@ -2,14 +2,18 @@ import asyncio
 import requests
 import os
 
-HUGGINGFACE_PING_URL = os.getenv("HUGGING_FACE_API_URL")
-API_KEY = os.getenv("HUGGING_FACE_API_KEY")
-
 
 def ping_huggingface_model():
     try:
+        HUGGINGFACE_PING_URL = os.getenv("HUGGING_FACE_API_URL")
+        API_KEY = os.getenv("HUGGING_FACE_API_KEY")
+
         headers = {"Authorization": f"Bearer {API_KEY}"}
         data = {"inputs": "ping"}
+        if not HUGGINGFACE_PING_URL:
+            print("HuggingFace URL 환경변수가 설정되지 않았습니다.")
+            return
+
         requests.post(HUGGINGFACE_PING_URL, headers=headers, json=data)
     except Exception as error:
         print(f"HuggingFace 요청에 실패 했습니다. {error}")
